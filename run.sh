e4fd08860d2544f9c6e0def19ddea64ae6fb1567
Shu Liu <lshu@berkeley.edu>
2025-10-13 11:17:00 -0700

    Doc Update (#6)

--- run.sh
+++
@@ -1,6 +0,0 @@
-python -m openevolve.cli \
-  openevolve/examples/ADRS/cloudcast/initial_program.py \
-  openevolve/examples/ADRS/cloudcast/evaluator.py \
-  --config openevolve/examples/ADRS/cloudcast/config.yaml \
-  --iterations 100 \
-  --output openevolve/examples/ADRS/cloudcast/full_output
